312eb31319db4d25ef5dfc12399f19a9833552a4
Fluoroid <fluoroid111@gmail.com>
2024-03-29 21:35:27 +0900

    import拡張子の修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,7 @@ This software is released under the MIT License, see LICENSE.
 This website contents (docs, images...) are released under the CC BY-NC-ND 4.0 License, see LICENSE.
 */
 
-import "@/styles/globals.css";
+import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
