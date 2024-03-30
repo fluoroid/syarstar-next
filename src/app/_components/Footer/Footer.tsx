5acc2d463974a28d081e1d447c469d2358c33693
Fluoroid <fluoroid111@gmail.com>
2024-03-30 23:17:14 +0900

    フッターの整形

--- src/app/_components/Footer/Footer.tsx
+++ src/app/_components/Footer/Footer.tsx
@@ -4,9 +4,18 @@ This software is released under the MIT License, see LICENSE.
 This website contents (docs, images...) are released under the CC BY-NC-ND 4.0 License, see LICENSE.
 */
 
-import styles from "@/app/_components/Header/Footer.module.scss";
+import styles from "@/app/_components/Footer/Footer.module.scss";
 import Image from "next/image";
 
+// 現在の年を取得
+const now = new Date();
+const year = now.getFullYear();
+
 export const Footer = () => {
-  return <footer>Foot</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyRight}>&copy;{year} Syar Star Observatory</p>
+      <p className={styles.legalInfo}>Legal</p>
+    </footer>
+  );
 };
