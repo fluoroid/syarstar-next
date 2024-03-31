9eed87420c3538d738126e3c78123e1e7222b612
Fluoroid <fluoroid111@gmail.com>
2024-03-31 21:24:07 +0900

    Font Awesome導入と外部リンクのコンポーネント化

---
+++ src/app/_components/FontAwesome/FontAwesome.tsx
@@ -0,0 +1,17 @@
+/*! 
+Copyright (c) 2024 Fluoroid (Syar Star Observatory)
+This software is released under the MIT License, see LICENSE.
+This website contents (docs, images...) are released under the CC BY-NC-ND 4.0 License, see LICENSE.
+*/
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+const ExternalLinkIcon = () => {
+  return <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="1x" />;
+};
+
+export default ExternalLinkIcon;
