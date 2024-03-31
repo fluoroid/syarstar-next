3984474767e3ee05cf34288228c95f6af3c84b69
Fluoroid <fluoroid111@gmail.com>
2024-03-31 19:39:33 +0900

    aboutページのスタイリング

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,7 +8,7 @@ import type { Metadata } from "next";
 import { metadata } from "@/app/layout";
 import { siteName } from "@/app/layout";
 import Image from "next/image";
-import styles from "@/app/page.module.scss";
+import styles from "@/app/about/page.module.scss";
 import {
   BreadCrumbMarkup,
   createBreadcrumbJsonLd,
@@ -52,7 +52,71 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 const About = () => {
   return (
     <>
-      <main className={styles.main}>aboutmainです</main>
+      <main className={styles.main}>
+        <h1>
+          <ruby>
+            眠星<rp>(</rp>
+            <rt>ねむりぼし</rt>
+            <rp>)</rp>
+          </ruby>
+          観測所
+        </h1>
+        <ul>
+          <li>
+            こんにちは！　「ふるおろいど」と申します。このページを見てくださり、ありがとうございます。
+          </li>
+          <li>
+            このサイトは、自身の創作活動の記録を残すために立てた小さな小さな観測所です。
+          </li>
+          <li>
+            同人活動はしていませんが、一応「眠星観測所」が個人サークル名みたいなかんじです。
+          </li>
+          <li>
+            うちの子の資料置き場と、イラストの保管庫として運用しようと思います。
+          </li>
+        </ul>
+        <h1>私について</h1>
+        <ul>
+          <li>
+            「ふるおろいど」という名前で創作活動しています。略して「ふるおろ」。
+          </li>
+          <li>誕生日：11月1日</li>
+          <li>生きているところ：千葉、眠星</li>
+          <li>好物：おさかな、チョコレート、和菓子。甘党。</li>
+          <li>お絵描きを中心とした創作が好き。ゲームやアニメも好きです。</li>
+          <li>絵</li>
+          <ul>
+            <li>
+              主にオリジナル(うちの子)や版権のファンアートを描いています。
+            </li>
+            <li>全年齢向け</li>
+            <li>たまによその子のファンアートも描くこともあるよ。</li>
+            <li>
+              一次創作だと、色彩豊かなデフォルメっぽい絵や、他者様の一次創作、オリジナルキャラクターが好きです。
+            </li>
+            <li>
+              版権だと、きらら系、日常系、東方や心温まるお話とかが好きです。
+            </li>
+          </ul>
+          <li>ゲーム</li>
+          <ul>
+            <li>
+              世界樹の迷宮、東方Project(弾幕STG)、星のカービィ、ぷよぷよなど。
+            </li>
+            <li>最近はSteamのインディーゲームとかもたまにやります。</li>
+            <li>オンラインゲームは、最近は原神しかやってないです……</li>
+            <li>
+              過去に、Sky
+              星を紡ぐ子どもたち、ぷよぷよ!!クエスト、きららファンタジアとかやってました。
+            </li>
+          </ul>
+          <li>アニメ</li>
+          <ul>
+            <li>きらら系、日常系などほっこりする内容を中心に見ています。</li>
+            <li>たまに放送中のTVアニメのファンアートも描いているよ。</li>
+          </ul>
+        </ul>
+      </main>
       <BreadCrumbMarkup data={jsonLd} />
     </>
   );
