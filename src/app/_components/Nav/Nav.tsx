93fb74e365423fc08f2463d1db4d78d70c5bc44b
Fluoroid <fluoroid111@gmail.com>
2024-04-18 00:00:49 +0900

    ナビゲーションのmap関数化

--- src/app/_components/Nav/Nav.tsx
+++ src/app/_components/Nav/Nav.tsx
@@ -8,75 +8,67 @@ import styles from "@/app/_components/Nav/Nav.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 
+// ナビゲーションデータ
+type navItems = {
+  id: number;
+  href: string;
+  src: string;
+  text: string;
+};
+const NAVITEMS: navItems[] = [
+  {
+    id: 1,
+    href: "./about",
+    src: "/img/star.png",
+    text: "About",
+  },
+  {
+    id: 2,
+    href: "",
+    src: "/img/star.png",
+    text: "Discover",
+  },
+  {
+    id: 3,
+    href: "./characters",
+    src: "/img/star.png",
+    text: "うちの子",
+  },
+  {
+    id: 4,
+    href: "./works",
+    src: "/img/star.png",
+    text: "作品",
+  },
+  {
+    id: 5,
+    href: "./link",
+    src: "/img/star.png",
+    text: "リンク",
+  },
+];
+
 export const Nav = () => {
   return (
     <nav className={styles.nav}>
       <ul>
-        <Link href="./about">
-          <li>
-            <Image
-              className={styles.navIcon}
-              src="/img/star.png"
-              alt="icon"
-              width={32}
-              height={32}
-              priority
-            />
-            <span className={styles.navText}>About</span>
-          </li>
-        </Link>
-        <Link href="">
-          <li>
-            <Image
-              className={styles.navIcon}
-              src="/img/star.png"
-              alt="icon"
-              width={32}
-              height={32}
-              priority
-            />
-            <span className={styles.navText}>Discover</span>
-          </li>
-        </Link>
-        <Link href="./characters">
-          <li>
-            <Image
-              className={styles.navIcon}
-              src="/img/star.png"
-              alt="icon"
-              width={32}
-              height={32}
-              priority
-            />
-            <span className={styles.navText}>うちの子</span>
-          </li>
-        </Link>
-        <Link href="./works">
-          <li>
-            <Image
-              className={styles.navIcon}
-              src="/img/star.png"
-              alt="icon"
-              width={32}
-              height={32}
-              priority
-            />
-            <span className={styles.navText}>作品</span>
-          </li>
-        </Link>
-        <Link href="./link">
-          <li>
-            <Image
-              className={styles.navIcon}
-              src="/img/star.png"
-              alt="icon"
-              width={32}
-              height={32}
-              priority
-            />
-            <span className={styles.navText}>リンク</span>
-          </li>
-        </Link>
+        {NAVITEMS.map((item) => {
+          return (
+            <Link key={item.id} href={item.href}>
+              <li>
+                <Image
+                  className={styles.navIcon}
+                  src={item.src}
+                  alt="icon"
+                  width={32}
+                  height={32}
+                  priority
+                />
+                <span className={styles.navText}>{item.text}</span>
+              </li>
+            </Link>
+          );
+        })}
       </ul>
     </nav>
   );
